4807ce2aa2486e5aebe2cb646ea36f986949137f
Fayzullo <abdunazarovfayzullo18@gmail.com>
2026-05-17 23:27:46 +0500

    fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = 'https://medical-system-production-a609.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE,
